9b9d6b5e43ca1cdf711db2bad5bf6fd09de38908
Adan Ferguson <adanferguson@gmail.com>
2022-03-02 22:03:00 -0700

    Scaled Factors

---
+++ game/scaledFactor.js
@@ -0,0 +1,18 @@
+export default class ScaledValue{
+
+  constructor(scalePct){
+    this.scalePct = scalePct
+  }
+
+  getVal(iterations){
+    return Math.pow(this.scalePct, iterations)
+  }
+
+  getFactors(val){
+    return baseLog(this.scalePct, val)
+  }
+}
+
+function baseLog(x, y){
+  return Math.log(y) / Math.log(x)
+}
