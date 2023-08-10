1553a7a458d5753d45c84d81c34fa91ed923ec13
Adan Ferguson <adanferguson@gmail.com>
2023-08-09 22:22:07 -0600

    Update notes

--- client/js/components/header.js
+++ client/js/components/header.js
@@ -12,7 +12,7 @@ const HTML = `
   <button class="back-button"><i class="fa-solid fa-arrow-left"></i></button>
   <div class="flex-rows">
     <a class="autocrawl clickable" href="/game">AUTOCRAWL</a>
-    <a class="version" href="/notes/0-8-0">v0.8.0.2</a>
+    <a class="version" href="/notes/0-8-0">v0.8.0.3</a>
   </div>
 </div>
 <div class="right-side">
