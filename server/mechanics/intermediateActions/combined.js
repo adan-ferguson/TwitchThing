278e64d731ffea9b69a2bca71424977986ce3e34
Adam Ferguson <adanferguson@gmail.com>
2023-08-30 00:10:38 -0600

    push deploy

--- server/mechanics/intermediateActions/combined.js
+++ server/mechanics/intermediateActions/combined.js
@@ -1,9 +1,9 @@
-import elementalBreath from './chimera/elementalBreath.js'
-import furiousStrikes from './chimera/furiousStrikes.js'
 import maybe from './common/maybe.js'
 import random from './common/random.js'
 import returnDamage from './common/returnDamage.js'
 import targetScaledAttack from './common/targetScaledAttack.js'
+import elementalBreath from './chimera/elementalBreath.js'
+import furiousStrikes from './chimera/furiousStrikes.js'
 import breakItem from './monster/breakItem.js'
 import explode from './monster/explode.js'
 import fireSpiritExplode from './monster/fireSpiritExplode.js'
@@ -11,15 +11,15 @@ import mushroomSpores from './monster/mushroomSpores.js'
 import painTrain from './monster/painTrain.js'
 import terribleCurse from './monster/terribleCurse.js'
 import terribleSituation from './monster/terribleSituation.js'
-import theBountyCollectorKill from './rogue/theBountyCollectorKill.js'
 import balancedSmite from './paladin/balancedSmite.js'
 import penance from './paladin/penance.js'
 import shieldBash from './paladin/shieldBash.js'
 import shieldsUp from './paladin/shieldsUp.js'
 import spikedShield from './paladin/spikedShield.js'
-export default { elementalBreath: { def: elementalBreath, id: 'elementalBreath', group: 'chimera' },furiousStrikes: { def: furiousStrikes, id: 'furiousStrikes', group: 'chimera' },maybe: { def: maybe, id: 'maybe', group: 'common' },random: { def: random, id: 'random', group: 'common' },returnDamage: { def: returnDamage, id: 'returnDamage', group: 'common' },targetScaledAttack: { def: targetScaledAttack, id: 'targetScaledAttack', group: 'common' },breakItem: { def: breakItem, id: 'breakItem', group: 'monster' },explode: { def: explode, id: 'explode', group: 'monster' },fireSpiritExplode: { def: fireSpiritExplode, id: 'fireSpiritExplode', group: 'monster' },mushroomSpores: { def: mushroomSpores, id: 'mushroomSpores', group: 'monster' },painTrain: { def: painTrain, id: 'painTrain', group: 'monster' },terribleCurse: { def: terribleCurse, id: 'terribleCurse', group: 'monster' },terribleSituation: { def: terribleSituation, id: 'terribleSituation', group: 'monster' },theBountyCollectorKill: { def: theBountyCollectorKill, id: 'theBountyCollectorKill', group: 'rogue' },balancedSmite: { def: balancedSmite, id: 'balancedSmite', group: 'paladin' },penance: { def: penance, id: 'penance', group: 'paladin' },shieldBash: { def: shieldBash, id: 'shieldBash', group: 'paladin' },shieldsUp: { def: shieldsUp, id: 'shieldsUp', group: 'paladin' },spikedShield: { def: spikedShield, id: 'spikedShield', group: 'paladin' } }
-export const chimera = { elementalBreath: { def: elementalBreath, id: 'elementalBreath', group: 'chimera' },furiousStrikes: { def: furiousStrikes, id: 'furiousStrikes', group: 'chimera' } }
+import theBountyCollectorKill from './rogue/theBountyCollectorKill.js'
+export default { maybe: { def: maybe, id: 'maybe', group: 'common' },random: { def: random, id: 'random', group: 'common' },returnDamage: { def: returnDamage, id: 'returnDamage', group: 'common' },targetScaledAttack: { def: targetScaledAttack, id: 'targetScaledAttack', group: 'common' },elementalBreath: { def: elementalBreath, id: 'elementalBreath', group: 'chimera' },furiousStrikes: { def: furiousStrikes, id: 'furiousStrikes', group: 'chimera' },breakItem: { def: breakItem, id: 'breakItem', group: 'monster' },explode: { def: explode, id: 'explode', group: 'monster' },fireSpiritExplode: { def: fireSpiritExplode, id: 'fireSpiritExplode', group: 'monster' },mushroomSpores: { def: mushroomSpores, id: 'mushroomSpores', group: 'monster' },painTrain: { def: painTrain, id: 'painTrain', group: 'monster' },terribleCurse: { def: terribleCurse, id: 'terribleCurse', group: 'monster' },terribleSituation: { def: terribleSituation, id: 'terribleSituation', group: 'monster' },balancedSmite: { def: balancedSmite, id: 'balancedSmite', group: 'paladin' },penance: { def: penance, id: 'penance', group: 'paladin' },shieldBash: { def: shieldBash, id: 'shieldBash', group: 'paladin' },shieldsUp: { def: shieldsUp, id: 'shieldsUp', group: 'paladin' },spikedShield: { def: spikedShield, id: 'spikedShield', group: 'paladin' },theBountyCollectorKill: { def: theBountyCollectorKill, id: 'theBountyCollectorKill', group: 'rogue' } }
 export const common = { maybe: { def: maybe, id: 'maybe', group: 'common' },random: { def: random, id: 'random', group: 'common' },returnDamage: { def: returnDamage, id: 'returnDamage', group: 'common' },targetScaledAttack: { def: targetScaledAttack, id: 'targetScaledAttack', group: 'common' } }
+export const chimera = { elementalBreath: { def: elementalBreath, id: 'elementalBreath', group: 'chimera' },furiousStrikes: { def: furiousStrikes, id: 'furiousStrikes', group: 'chimera' } }
 export const monster = { breakItem: { def: breakItem, id: 'breakItem', group: 'monster' },explode: { def: explode, id: 'explode', group: 'monster' },fireSpiritExplode: { def: fireSpiritExplode, id: 'fireSpiritExplode', group: 'monster' },mushroomSpores: { def: mushroomSpores, id: 'mushroomSpores', group: 'monster' },painTrain: { def: painTrain, id: 'painTrain', group: 'monster' },terribleCurse: { def: terribleCurse, id: 'terribleCurse', group: 'monster' },terribleSituation: { def: terribleSituation, id: 'terribleSituation', group: 'monster' } }
-export const rogue = { theBountyCollectorKill: { def: theBountyCollectorKill, id: 'theBountyCollectorKill', group: 'rogue' } }
 export const paladin = { balancedSmite: { def: balancedSmite, id: 'balancedSmite', group: 'paladin' },penance: { def: penance, id: 'penance', group: 'paladin' },shieldBash: { def: shieldBash, id: 'shieldBash', group: 'paladin' },shieldsUp: { def: shieldsUp, id: 'shieldsUp', group: 'paladin' },spikedShield: { def: spikedShield, id: 'spikedShield', group: 'paladin' } }
+export const rogue = { theBountyCollectorKill: { def: theBountyCollectorKill, id: 'theBountyCollectorKill', group: 'rogue' } }
